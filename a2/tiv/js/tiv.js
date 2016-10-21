aa1187fb3ec920ccacdb4043c69e351a41154419
Tasos Anastasas <csd3166@csd.uoc.gr>
2016-10-22 01:20:48 +0300

    2) Implement the 3 basic APIs for TIV and also add a simple buttons on implementation of TIV from ex01. Removed the unused code. Shadow will be fixed on next commit

--- a2/tiv/js/tiv.js
+++ a2/tiv/js/tiv.js
@@ -3,23 +3,47 @@ var TIV3166 = function () {
     
     var loadedImages = {
         array: []
+    }, images2display = {
+        array: []
     };
+    
+    function checkImg(name) {
+        return (name.match(/\.(jpeg|jpg|gif|png)$/) !==  null);
+    }
+    
     return {
-        //read a directory and if exist take all images and put
-        //them on loadedImages.array with push
         loadImages: function () {
-            
+            var files = document.getElementById("images").files, i, file, reader, div;
+            for (i = 0; i < files.length; i += 1) {
+                file = files[i];
+                loadedImages.array.push(file.name);
+                reader = new FileReader();
+                if (checkImg(file.name)) {
+                    reader.onload = (function (file) {
+                        return function (e) {
+                            div = document.createElement('div');
+                            div.className = "tile";
+                            div.innerHTML = ['<img  src="', e.target.result, '" title="', escape(file.name), '">'].join('');
+                            images2display.array.push(div);
+                        };
+                    })(file);
+                }
+                reader.readAsDataURL(file);
+            }
         },
         //return all the elements from the array
         getLoadedImages: function () {
-            
+            return loadedImages.array;
         },
         //using the tile from previous TIV html on ex01
         //drawing images inside on elem that user give
         //on error add
         //<h1>CANT LOAD THE IMAGE FROM THE COLLECTION</h1>
         showLoadedImages: function (elem) {
-        
+            var i;
+            for (i = 0; i < images2display.array.length; i += 1) {
+                document.getElementById(elem).insertBefore(images2display.array[i], null);
+            }
         }
     };
-};
+}();
