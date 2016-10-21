8e86b4acff6c7e45037710837641c6b8f074ca49
Tasos Anastasas <csd3166@csd.uoc.gr>
2016-10-21 19:35:26 +0300

    1) Added the basic structure for TIV api

---
+++ a2/tiv/js/tiv.js
@@ -0,0 +1,25 @@
+var TIV3166 = function () {
+    "use strict";
+    
+    var loadedImages = {
+        array: []
+    };
+    return {
+        //read a directory and if exist take all images and put
+        //them on loadedImages.array with push
+        loadImages: function () {
+            
+        },
+        //return all the elements from the array
+        getLoadedImages: function () {
+            
+        },
+        //using the tile from previous TIV html on ex01
+        //drawing images inside on elem that user give
+        //on error add
+        //<h1>CANT LOAD THE IMAGE FROM THE COLLECTION</h1>
+        showLoadedImages: function (elem) {
+        
+        }
+    };
+};
