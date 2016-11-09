2f6f81ce570ca4cb1909c2ebe8b4729065772ad3
Tasos Anastasas <csd3166@csd.uoc.gr>
2016-11-09 12:59:42 +0200

    Added html,css,js with basic containers and some buttons

---
+++ a3/HY359-EX03/web/user_action.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function register_action() {
+    "use strict";
+}
+
+function login_action() {
+    "use strict";
+}
+
+function logout_action() {
+    "use strict";
+}
+
+function formValidation() {
+    "use strict";
+}
