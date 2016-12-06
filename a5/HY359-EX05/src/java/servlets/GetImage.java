a8c1a1b9bb90357dd38541138e6c6b5482e6aeaa
Giwrgos Giakoumis <csd3157@csd.uoc.gr>
2016-12-07 01:05:27 +0200

    [UPDATE] data base, [ADD] GetImage servlet
    Implement GetImage only with metadata false

---
+++ a5/HY359-EX05/src/java/servlets/GetImage.java
@@ -0,0 +1,36 @@
+package servlets;
+
+import cs359db.db.PhotosDB;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Giakoumis Giwrgos
+ */
+public class GetImage extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        String photoId = request.getParameter("image");
+        String wantMetadata = request.getParameter("metadata");
+
+        try {
+            if (wantMetadata.equals("true")) {
+                // TODO implement give metadata functionality
+            } else {
+                byte[] blob = PhotosDB.getPhotoBlobWithID(Integer.parseInt(photoId));
+                ServletOutputStream out = response.getOutputStream();
+                out.write(blob);
+            }
+        } catch (ClassNotFoundException ex) {
+            System.out.println("servlets.GetImage.doPost(): " + ex.getMessage());
+        }
+    }
+}
