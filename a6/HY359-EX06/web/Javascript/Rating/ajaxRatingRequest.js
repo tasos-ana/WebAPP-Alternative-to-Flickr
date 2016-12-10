2ba008b9513050411ff744f9f6fa588d8a0a7e6c
Tasos Anastasas <csd3166@csd.uoc.gr>
2016-12-10 17:47:40 +0200

    [ADD] files for rating

---
+++ a6/HY359-EX06/web/Javascript/Rating/ajaxRatingRequest.js
@@ -0,0 +1,8 @@
+/* 
+ *     Document      :ajaxRatingRequest.js
+ *     Project       :HY359-EX06
+ *     Author        :Tasos198
+ *     Created on    :Dec 10, 2016
+ */
+
+
---
+++ a6/HY359-EX06/web/Javascript/Rating/rating_action.js
@@ -0,0 +1,8 @@
+/* 
+ *     Document      :rating_action.js
+ *     Project       :HY359-EX06
+ *     Author        :Tasos198
+ *     Created on    :Dec 10, 2016
+ */
+
+
